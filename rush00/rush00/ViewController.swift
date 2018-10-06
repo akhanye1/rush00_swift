401a47811e3be614d284c597c6f4486629695a1b
Archiebald Katleho KHANYE <akhanye@e4r24p1.wethinkcode.co.za>
2018-10-06 11:23:28 +0200

    Initializing

---
+++ rush00/rush00/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rush00
+//
+//  Created by Archiebald Katleho KHANYE on 2018/10/06.
+//  Copyright © 2018 Archiebald Katleho KHANYE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
