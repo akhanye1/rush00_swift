98d0628e454038ac2fb81479d4234716e9116e09
Laura MAHLANGU <lmahlang@e4r23p2.wethinkcode.co.za>
2018-10-06 13:50:56 +0200

    updating info

---
+++ rush00/rush00/Views/TopicCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopicCell.swift
+//  rush00
+//
+//  Created by Laura MAHLANGU on 2018/10/06.
+//  Copyright © 2018 Archiebald Katleho KHANYE. All rights reserved.
+//
+
+import UIKit
+
+class TopicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- rush00/rush00/Views/TopicView.swift
+++ rush00/rush00/Views/TopicView.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class TopicView: UIViewController {
+class TopicView: UIViewController,UITableViewDataSource,UITableViewDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var logout: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +22,11 @@ class TopicView: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
     
 
     /*
